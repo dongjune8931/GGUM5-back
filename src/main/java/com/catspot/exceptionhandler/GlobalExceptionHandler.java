ad4e1a39f50ff1bd608e9d99ed82945e0ac77797
unluckyboy214 <janggoon0214@naver.com>
2024-10-23 22:50:31 +0900

    [FEAT] 예외처리 로직 추가(IOException)

--- src/main/java/com/catspot/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/catspot/exceptionhandler/GlobalExceptionHandler.java
@@ -1,14 +1,25 @@
 package com.catspot.exceptionhandler;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler{
+    // IOException 처리
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorResponse> IOExceptionHandler(IOException ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
+    // 커스텀 예외 처리
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> CustomExceptionHandler(CustomException ex){
         ErrorCode errorCode = ex.getErrorCode();
@@ -19,7 +30,7 @@ public class GlobalExceptionHandler{
         return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
     }
 
-
+    // 전체적인 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> AllExceptionHandler(Exception ex){
         ErrorResponse errorResponse = new ErrorResponse(
