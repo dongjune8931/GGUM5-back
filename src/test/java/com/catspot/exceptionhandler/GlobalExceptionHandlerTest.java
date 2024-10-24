8ddb641c8437d37f4b885e002c62d4265233f9eb
unluckyboy214 <janggoon0214@naver.com>
2024-10-24 23:48:23 +0900

    [TEST] 예외 처리 테스트 추가

--- src/test/java/com/catspot/exceptionhandler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/catspot/exceptionhandler/GlobalExceptionHandlerTest.java
@@ -17,7 +17,7 @@ public class GlobalExceptionHandlerTest {
     private final GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
 
     @Test
-    public void testGlobalExceptionHandler(){
+    public void testGlobalException(){
         // Given
         Exception exception = new Exception("General Exception occurred");
 
@@ -31,4 +31,37 @@ public class GlobalExceptionHandlerTest {
         assertThat(responseEntity.getBody().getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
         assertThat(responseEntity.getBody().getMessage()).isEqualTo("General Exception occurred");
     }
+
+    @Test
+    public void test_INVALID_PARAMETER_Exception() {
+        // Given
+        CustomException customException = new CustomException(CommonErrorCode.INVALID_PARAMETER);
+
+        // When
+        ResponseEntity<ErrorResponse> responseEntity = globalExceptionHandler.CustomExceptionHandler(customException);
+
+        // Then
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody().getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo("Invalid parameter included");
+    }
+
+    @Test
+    public void test_RESOURCE_NOT_FOUND_Exception() {
+        // Given
+        CustomException customException = new CustomException(CommonErrorCode.RESOURCE_NOT_FOUND);
+
+        // When
+        ResponseEntity<ErrorResponse> responseEntity = globalExceptionHandler.CustomExceptionHandler(customException);
+
+        // Then
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody().getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo("Resource not exists");
+    }
+
 }
