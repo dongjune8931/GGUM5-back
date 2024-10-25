3c5a9042c5cd8b4aefaf135f00a991becdc41c3e
SolfE <love23869318@gmail.com>
2024-10-25 12:04:08 +0900

    [REFACTOR] Crawler 디렉터리 구조 변경에 따른 패키지 변경

--- src/main/java/com/catspot/crawler/StudyPlaceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.catspot.crawler;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudyPlaceRepository extends JpaRepository<StudyPlace, Long> {
-}
